555838809e4a864d26f34e1c7aaeac99ea93a300
Mattias <145386702+MattiasGijsels@users.noreply.github.com>
2023-10-21 16:23:49 +0200

    added file to repo

---
+++ Labo 18-10 oplossing oef 1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Labo_18_10_oplossing_oef_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            while (isRunning)
+            {
+
+                Console.Write(">");
+                string input = Console.ReadLine() ?? "";
+                switch (input.ToLower())
+                {
+                    case "exit":
+                    case "quit":
+                        isRunning = false;
+                        break;
+                    case "echo":
+                        Console.WriteLine($"You Wite '{input}");
+                        break;
+                    case "clear":
+                        Console.Clear();
+                        break;
+
+                    default:
+                       
+                        ShowError($"The following command is wrong {input}");
+                        // f11 step into
+                        break;
+
+
+                }
+
+
+
+
+
+            }
+        }
+       
+        static void ShowError(string error) 
+        {
+            Console.WriteLine($"You Wrote the following error" + Console.ReadLine());
+            
+
+
+        }
+        
+    }
+}
