29cc4d0bfb9350ff3ed28aa083370ce8c4626e1c
Mattias Gijsels <mattias.gijsels@hotmail.com>
2023-10-22 21:11:00 +0200

    color changes custom cursor tries method newCursor still iffy

--- Labo 18-10 oplossing oef 1/Program.cs
+++ Labo 18-10 oplossing oef 1/Program.cs
@@ -5,47 +5,58 @@
         static void Main(string[] args)
         {
             bool isRunning = true;
+            string[] cursorChoice = { "$ " , "* ", "ù ", "£ " };
+
             while (isRunning)
             {
-
-                Console.Write(">");
-                string input = Console.ReadLine() ?? "";
-                switch (input.ToLower())
+                string customCursor = "> ";
+                Console.Write(customCursor);
+                string userInput = Console.ReadLine() ?? "";
+                switch (userInput.ToLower())
                 {
                     case "exit":
                     case "quit":
                         isRunning = false;
                         break;
+
                     case "echo":
-                        Console.WriteLine($"You Wrote '{input}");
+                        Console.WriteLine($"You Wrote '{userInput}'");
                         break;
+
                     case "clear":
                         Console.Clear();
                         break;
 
-                    default:
-                       
-                        ShowError($"The following command is wrong {input}");
-                        // f11 step into
+                    case "change the cursor":
+                        Console.WriteLine($"To change your cursor, type one of the following symbols ");
+                    foreach (string choice in cursorChoice)
+                    {
+                        Console.WriteLine(choice);
+                    }
+                        Console.ReadLine();
+                        Console.WriteLine(ChangeCursor(userInput));
                         break;
-                         etz
 
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(ShowError(userInput));
+                        Console.ResetColor(); 
+                        break;
+                      
                 }
-
-
-
-
 
             }
         }
        
-        static void ShowError(string error) 
+        static string ShowError(string error) 
+        {   
+            return ($"You Wrote the following error '{error}'");
+        }
+        static string ChangeCursor (string newCursor) 
         {
-            Console.WriteLine($"You Wrote the following error" + Console.ReadLine());
-            
-
+            newCursor = Console.ReadLine();
+            return newCursor;
 
         }
-        
     }
 }
